9d7027c1b4400a181e55c3292a2b460c7dc4eef1
ShreyasSathyanarayana <150839398+ShreyasSathyanarayana@users.noreply.github.com>
2024-06-26 15:00:42 +0530

    Initial commit

---
+++ src/screen/ConversionScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ConversionScreen = () => {
+  return (
+    <View>
+      <Text>ConversionScreen</Text>
+    </View>
+  )
+}
+
+export default ConversionScreen
